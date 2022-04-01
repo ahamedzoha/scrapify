bde2dae1c668600f496eb6cf9c6131e8f03ca3c8
ahamedzoha <ahamed.zoha@gmail.com>
2022-04-01 23:55:18 +0600

    added scraping lgic

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+const URL = `https://www.bproperty.com/en/bangladesh/properties-for-rent/`
+
+axios
+  .get(URL)
+  .then((response) => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const linksArray = []
+    $(".ef447dde", html).each(function () {
+      $(this).text()
+      const links = `${URL.split("com")[0]}com` + $(this).find("a").attr("href")
+      const title = $(this).find("a").attr("title")
+      //   const title = $(this).find("span").
+      linksArray.push({
+        title,
+        links,
+      })
+    })
+    console.log(linksArray)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
