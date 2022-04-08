297e46dca5f9a47a8a1532019b3cd081d356e822
ahamedzoha <ahamed.zoha@gmail.com>
2022-04-08 16:53:03 +0600

    added async to get All stock Tickers

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -1,10 +1,52 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+
+const URL = `https://www.dsebd.org/`
+
 // @author - Azaz
 // @desc  Get all stock tickers
 // @route GET /api/stocks
-const getAllStockTickers = (req, res) => {
-  res.status(200).json({
-    message: "Welcome to the Stock API",
-  })
+const getAllStockTickers = async (req, res) => {
+  const stockData = []
+
+  await axios
+    .get(URL)
+    .then((response) => {
+      const html = response.data
+
+      const $ = cheerio.load(html)
+
+      $(".abhead", html).each(function () {
+        $(this).text()
+
+        const companyName = $(this)
+          .text()
+          .replace(/\t/g, "")
+          .split(" ")[0]
+          .trim()
+          .match(/\d?[a-zA-Z]|\([^)]*\)/g)
+          .join("")
+
+        const priceData = $(this)
+          .text()
+          .match(/[+/-]?[0-9]+\.[0-9]+/g)
+
+        stockData.push({
+          name: companyName,
+          prices: {
+            current: priceData[0],
+            changed: priceData[1],
+            changePercent: `${priceData[2]}%`,
+          },
+        })
+      })
+      console.log(stockData)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  res.status(200).json(stockData)
 }
 
 // @author - Azaz
--- index.js
+++ index.js
@@ -1,53 +1,8 @@
 const express = require("express")
-const axios = require("axios")
-const cheerio = require("cheerio")
 
 const PORT = process.env.PORT || 8000
 
 const app = express()
-
-const URL = `https://www.dsebd.org/`
-
-const stockData = []
-
-const getStockData = () => {
-  axios
-    .get(URL)
-    .then((response) => {
-      const html = response.data
-
-      const $ = cheerio.load(html)
-
-      $(".abhead", html).each(function () {
-        $(this).text()
-
-        const companyName = $(this)
-          .text()
-          .replace(/\t/g, "")
-          .split(" ")[0]
-          .trim()
-          .match(/\d?[a-zA-Z]|\([^)]*\)/g)
-          .join("")
-
-        const priceData = $(this)
-          .text()
-          .match(/[+/-]?[0-9]+\.[0-9]+/g)
-
-        stockData.push({
-          name: companyName,
-          prices: {
-            current: priceData[0],
-            changed: priceData[1],
-            changePercent: `${priceData[2]}%`,
-          },
-        })
-        console.log(stockData)
-      })
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-}
 
 app.use("/api/stocks", require("./routes/stockRoutes"))
 
