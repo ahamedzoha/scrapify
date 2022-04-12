21ac503477334c25c217a0473bfe2fd8c1b64015
ahamedzoha <ahamed.zoha@gmail.com>
2022-04-12 22:56:51 +0600

    added params

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -55,6 +55,7 @@ const getSpecificStockTicker = (req, res) => {
   //   res.status(200).json({
   //     message: `Welcome to the Stock API ${req.params.id}`,
   //   })
+  console.log(req.params.id)
   console.log(req.body)
   res.status(200).json({ message: `${req.body}` })
 }
