76c51a969af6b5143eab02ca0d6298189eddaa4e
ahamedzoha <ahamed.zoha@gmail.com>
2022-05-18 15:23:12 +0600

    added firebase configs

--- api/controllers/stockController.js
+++ api/controllers/stockController.js
@@ -1,65 +1,29 @@
 const axios = require("axios")
-const cheerio = require("cheerio")
+const { initializeApp } = require("firebase/app")
+const { getFirestore, getDocs, collection } = require("firebase/firestore")
+const firebaseConfig = require("../config")
 
-const URL = `https://www.dsebd.org/`
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
 
 // @author - Azaz
 // @desc  Get all stock tickers
 // @route GET /api/stocks
 const getAllStockTickers = async (req, res) => {
-  const stockData = []
-  await axios
-    .get(URL)
-    .then((response) => {
-      const html = response.data
+  let stockTickers = []
+  const Stocks = await getDocs(collection(db, "stocks"))
+  Stocks.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`)
+    stockTickers.push(doc.data())
+  })
 
-      const $ = cheerio.load(html)
-
-      $(".abhead", html).each(function () {
-        $(this).text()
-
-        const companyName = $(this)
-          .text()
-          .replace(/\t/g, "")
-          .split(" ")[0]
-          .trim()
-          .match(/\d?[a-zA-Z]|\([^)]*\)/g)
-          .join("")
-
-        const priceData = $(this)
-          .text()
-          .match(/[+/-]?[0-9]+\.[0-9]+/g)
-
-        stockData.push({
-          name: companyName,
-          prices: {
-            current: priceData[0],
-            changed: priceData[1],
-            changePercent: `${priceData[2]}%`,
-          },
-          raw: $(this).text(),
-        })
-      })
-      //   console.log(stockData)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-
-  res.status(200).json(stockData)
+  res.status(200).send(stockTickers)
 }
 
 // @author - Azaz
 // @desc  Get specific stock ticker based on ID (STOCK NAME)
 // @route GET /api/stocks
-const getSpecificStockTicker = (req, res) => {
-  //   res.status(200).json({
-  //     message: `Welcome to the Stock API ${req.params.id}`,
-  //   })
-  console.log(req.params.id)
-  console.log(req.body)
-  res.status(200).json({ message: `${req.body}` })
-}
+const getSpecificStockTicker = (req, res) => {}
 
 module.exports = {
   getAllStockTickers,
--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const cors = require("cors")
 
 const PORT = process.env.PORT || 8000
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 
 app.use("/api/stocks", require("./routes/stockRoutes"))
