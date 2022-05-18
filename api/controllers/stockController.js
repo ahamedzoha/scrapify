e44ee569a2e509d2ca2d7c7469fd9e0f44e70b00
ahamedzoha <ahamed.zoha@gmail.com>
2022-05-18 17:09:45 +0600

    added firebase query to get all stock tickers

--- api/controllers/stockController.js
+++ api/controllers/stockController.js
@@ -1,20 +1,33 @@
 const axios = require("axios")
 const { initializeApp } = require("firebase/app")
-const { getFirestore, getDocs, collection } = require("firebase/firestore")
+const {
+  getFirestore,
+  getDocs,
+  collection,
+  query,
+  orderBy,
+  limit,
+  where,
+} = require("firebase/firestore")
 const firebaseConfig = require("../config")
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
+const stocksRef = collection(db, "stocks")
 
 // @author - Azaz
 // @desc  Get all stock tickers
 // @route GET /api/stocks
 const getAllStockTickers = async (req, res) => {
-  let stockTickers = []
-  const Stocks = await getDocs(collection(db, "stocks"))
+  let stockTickers = { data: [], timestamp: null }
+
+  const q = query(stocksRef, orderBy("timestamp", "desc"), limit(1))
+
+  const Stocks = await getDocs(q)
   Stocks.forEach((doc) => {
     console.log(`${doc.id} => ${doc.data()}`)
-    stockTickers.push(doc.data())
+    stockTickers.data.push(doc.data().data)
+    stockTickers.timestamp = doc.data().timestamp.toDate()
   })
 
   res.status(200).send(stockTickers)
@@ -23,7 +36,25 @@ const getAllStockTickers = async (req, res) => {
 // @author - Azaz
 // @desc  Get specific stock ticker based on ID (STOCK NAME)
 // @route GET /api/stocks
-const getSpecificStockTicker = (req, res) => {}
+const getSpecificStockTicker = async (req, res) => {
+  let stockTicker = { data: [], timestamp: null }
+
+  const q = query(stocksRef, where("data", "array-contains", req.params.id))
+
+  const Stocks = await getDocs(q)
+  Stocks.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`)
+    stockTicker.data.push(doc.data().data)
+    stockTicker.timestamp = doc.data().timestamp.toDate()
+  })
+
+  // res.status(200).send({
+  //   sent: {
+  //     reqID: req.params.id,
+  //   },
+  // })
+  res.status(200).send(stockTicker)
+}
 
 module.exports = {
   getAllStockTickers,
