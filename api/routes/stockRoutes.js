152dc4daf79538f2ab9720e920add7bc95bd4bcf
ahamedzoha <ahamed.zoha@gmail.com>
2022-06-09 23:56:56 +0600

    working on new function

--- api/routes/stockRoutes.js
+++ api/routes/stockRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const cache = require("../routeCache")
+const { checkCompany } = require("./middlewares/stock_routes_middlewares")
 const {
   getAllStockTickers,
   getCompanyDetails,
--- functions/index.js
+++ functions/index.js
@@ -62,52 +62,61 @@ exports.getAllStockTickers = functions.pubsub
   })
 
 // testing function using HTTPS method
-exports.getAllStockTickersHTTP = functions.https.onRequest(async (req, res) => {
-  stockData = []
-  await axios
-    .get(URL)
-    .then(async (resp) => {
-      const html = resp.data
+exports.getAllStockTickersNameHTTP = functions.https.onRequest(
+  async (req, res) => {
+    TickerNames = []
+    const URI = "https://www.dsebd.org/latest_share_price_scroll_l.php"
+    await axios
+      .get(URI)
+      .then(async (resp) => {
+        const html = resp.data
 
-      const $ = cheerio.load(html)
+        const $ = cheerio.load(html)
+        const window = $(".row", html).html()
 
-      $(".abhead", html).each(function () {
-        $(this).text()
+        $(
+          "#RightBody > div.row > div.col-md-9.col-sm-9.col-xs-9.full-width-mobile > div.floatThead-wrapper > div.table-responsive.inner-scroll > table > tbody > tr > td:nth-child(2)"
+        ).each(function () {
+          $(this).text()
 
-        const companyName = $(this)
-          .text()
-          .replace(/\t/g, "")
-          .split(" ")[0]
-          .trim()
-          .match(/\d?[a-zA-Z]|\([^)]*\)/g)
-          .join("")
+          const companyName = $(this)
 
-        const priceData = $(this)
-          .text()
-          .match(/[+/-]?[0-9]+\.[0-9]+/g)
+          TickerNames.push(companyName)
+        })
 
-        stockData.push({
-          name: companyName,
-          prices: {
-            current: priceData[0],
-            changed: priceData[1],
-            changePercent: `${priceData[2]}%`,
-          },
+        // delete all documents from collection
+        await admin
+          .firestore()
+          .collection("stock_ids")
+          .get()
+          .then((snapshot) => {
+            snapshot.forEach((doc) => {
+              doc.ref.delete()
+            })
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+
+        // add all ticker names to collection
+        await admin
+          .firestore()
+          .collection("stock_ids")
+          .add({ ...TickerNames })
+
+        functions.logger.info("Ran Function Successfully", {
+          structuredData: true,
+        })
+        console.log(`Successfully scraped ${TickerNames.length} stocks`)
+
+        res.send({
+          length: JSON.stringify(TickerNames.length),
+          data: TickerNames,
         })
       })
-      await admin.firestore().collection("stocks").add({
-        timestamp: admin.firestore.FieldValue.serverTimestamp(),
-        data: stockData,
+      .catch((error) => {
+        functions.logger.error("Unsuccessful Run", { structuredData: true })
+        console.log(error)
       })
-
-      functions.logger.info("Ran Function Successfully", {
-        structuredData: true,
-      })
-      console.log(`Successfully scraped ${stockData.length} stocks`)
-      res.end(JSON.stringify(stockData.length))
-    })
-    .catch((error) => {
-      functions.logger.error("Unsuccessful Run", { structuredData: true })
-      console.log(error)
-    })
-})
+  }
+)
